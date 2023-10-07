6e6afaf21933d094def2b8d6a030f9304d9ae3ff
NghiaNguyenLeThanh <144520934+NghiaNguyenLeThanh@users.noreply.github.com>
2023-10-07 15:04:45 +0700

    first commit

---
+++ Tu_Hoc/modules.js
@@ -0,0 +1,10 @@
+//Modules
+const names = require('./name')
+const sayHi = require('./utils')
+const data = require('./alternative-favor')
+//console.log(data)
+require('./mind-genade')
+
+// sayHi('susan');
+// sayHi(names.john);
+// sayHi(names.peter);
